447c05527851cab9b793334fb2a3601e0a272410
Administrator <1161687912@qq.com>
2021-01-17 22:38:25 +0800

    hr修改

--- src/main/resources/static/js/admin/index.js
+++ src/main/resources/static/js/admin/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     var userId=$("#userId").val();
-
     $.ajax({
         url : "/employee/getMyName?userId="+userId,     //后台请求的数据
         async : true,                   //是否异步请求
